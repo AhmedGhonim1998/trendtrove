c3bf5f7dccd10153bf1b31c7b7e9a9a1fceb8f38
AhmedGhonim1998 <146675736+AhmedGhonim1998@users.noreply.github.com>
2024-06-28 23:26:04 +0300

    update

---
+++ src/components/Rating.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Rating = () => {
+    return (
+        <>
+        <span className='ratting'>
+            <i class="icofont-ui-rating"></i>
+            <i class="icofont-ui-rating"></i>
+            <i class="icofont-ui-rating"></i>
+            <i class="icofont-ui-rating"></i>
+            <i class="icofont-ui-rating"></i>
+            </span>
+        </>
+    )
+}
+
+export default Rating
