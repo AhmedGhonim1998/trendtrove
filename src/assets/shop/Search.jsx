3c13c404aba25925b389b1928593b1326d8ced94
AhmedGhonim1998 <146675736+AhmedGhonim1998@users.noreply.github.com>
2024-07-04 22:04:13 +0300

    update

--- src/assets/shop/Search.jsx
+++ src/assets/shop/Search.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-const Search = ({products , gridList}) => {
-    const[searchTrem, setSearchTrem]=useState("");
+const Search = ({ products, gridList }) => {
+    const [searchTrem, setSearchTrem] = useState("");
 
-    const filteredProducts = products.filter((product)=>(product.name.toLowerCase().includes(searchTrem.toLowerCase())));
+    const filteredProducts = products.filter((product) => (product.name.toLowerCase().includes(searchTrem.toLowerCase())));
     return (
         <div className='widget widget-search'>
             <form className='search-wrapper mb-3'>
-                <input type="text"  name='search' id='search' placeholder='search...' defaultValue={searchTrem} onChange={(e)=>setSearchTrem(e.target.value)}/>
+                <input type="text" name='search' id='search' placeholder='search...' defaultValue={searchTrem} onChange={(e) => setSearchTrem(e.target.value)} />
                 <button type='submit'>
                     <i className='icofont-search'></i>
                 </button>
@@ -16,18 +16,19 @@ const Search = ({products , gridList}) => {
             {/* showing search result */}
             <div>
                 {
-                    searchTrem && filteredProducts.map((product)=>(
+                    searchTrem && filteredProducts.map((product) => (
                         <Link key={product.id} to={`/shop/${product.id}`}>
                             <div className="d-flex gab-3 p-2">
                                 <div>
-                                    <div className="pro-thumb h-25">
-                                        <img src={product.img} alt="img" width={70} className='flex-{grow|shrink}-0'/>
+                                    <div className="pro-thumb h-25 imageFetch">
+                                        <img src={product.img} alt="img" width={70} className='flex-{grow|shrink}-0 imgContainer' />
                                     </div>
                                 </div>
                                 <div className="product-content">
-                                    <p className='ms-2'>
-                                        <Link to={`/shop/${product.id}`} className='text-decoration-none text-black'>{product.name}</Link>
+                                    <p className='ms-2 mb-0'>
+                                        <Link to={`/shop/${product.id}`} className='text-decoration-none text-black fs-6'>{product.name}</Link>
                                     </p>
+                                    <h6 className='text-decoration-none text-black mt-0 ms-2'>$ {product.price}</h6>
                                 </div>
                             </div>
                         </Link>
